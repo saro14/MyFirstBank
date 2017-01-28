ded1c06c590b6230a9c4deb4b5edcedc2050bf50
saro <saro@RAMANSOM-ASUS>
2017-01-28 11:02:28 -0800

    My first code check-in

---
+++ MyFirstBank/MyFirstBank/Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstBank
+{
+    /// <summary>
+    /// This class defines the account for a bank
+    /// This is the blueprint
+    /// 
+    /// </summary>
+    class Account
+    {
+        #region Statics
+        //This is just to maintain last account number
+        //If there is a static keyword, it is general-shared, not for every instance - not part of every account
+        private static int lastAccountNumber = 0;
+        #endregion
+        #region Properties
+        public int AccountNumber { get; private set; }
+        public string EmailAddress { get; set; }
+        //This private set is to just read the balance, not write to it.
+        public decimal Balance { get; private set; }
+        #endregion
+        #region Methods
+        //constructor - without this we can't call new
+        //this("", 0.0M) - empty..there are no parameters in this constructor when compared to the third one
+        public Account() : this("", 0.0M)
+        {
+            //this means go to the instance of the memory that you created
+            //this.AccountNumber = lastAccountNumber +=1;
+            //this.AccountNumber = ++lastAccountNumber;
+        }
+        //Method overloading
+        //this(emailAddress, 0.0M) - here the email address is given as the parameter, but no balance so that is empty parameter
+        public Account(string emailAddress): this(emailAddress, 0.0M)
+        {
+            //this.AccountNumber = ++lastAccountNumber;
+            //this.EmailAddress = emailAddress;
+        }
+        public Account(string emailAddress, decimal balance)
+        {
+            this.AccountNumber = ++lastAccountNumber;
+            this.EmailAddress = emailAddress;
+            this.Balance = balance;
+        }
+        //If you don't specify the access modifier, it will by default be private
+        public decimal Deposit(decimal amount)
+        {
+            //Balance = Balance+amount;
+            Balance += amount;
+            return Balance;
+        }
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+        #endregion
+    }
+}
---
+++ MyFirstBank/MyFirstBank/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstBank
+{
+    class Program
+    {
+        // Main is the entry point for the program to start
+        static void Main(string[] args)
+        {
+            //create an account object/instance
+            //new keyword - calling the constructor
+            var myAccount = new Account();
+            myAccount.EmailAddress = "test@test.com";
+            //myAccount.AccountNumber = 12345;
+            //Problem is allowing the user to set the balance
+            //myAccount.Balance = 1000000;
+            //M - stands for money
+            //var - stands for variable
+            myAccount.Deposit(200.50M);
+            //$ - to create placeholders
+            //C - result in currency format
+            Console.WriteLine($"The balance in my account - {myAccount.AccountNumber} is {myAccount.Balance:C}");
+
+            var myAccount2 = new Account("test2@test.com");
+            //myAccount2.EmailAddress = "test2@test.com";
+            myAccount2.Deposit(200.50M);
+            Console.WriteLine($"The balance in my account - {myAccount2.AccountNumber} is {myAccount2.Balance:C}");
+
+        }
+    }
+}
